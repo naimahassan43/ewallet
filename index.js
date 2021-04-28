2fe46179520d05dc2e02a1d00899773c739e2a58
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-04-28 20:53:58 +0100

    Get Formatted Time

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//UI
 document.querySelector('#ewallet-form').addEventListener('submit', function(e) {
     e.preventDefault();
     // console.log('submitted');
@@ -14,6 +15,7 @@ document.querySelector('#ewallet-form').addEventListener('submit', function(e) {
 });
 
 function addItems(type, desc, value) {
+    const time = getFormattedTime();
     const newHtml = `
                   <div class="item">
                     <div class="item-description-time">
@@ -21,7 +23,7 @@ function addItems(type, desc, value) {
                         <p>${desc}</p>
                       </div>
                       <div class="item-time">
-                        <p>25 Feb, 06:45 PM</p>
+                        <p>${time}</p>
                       </div>
                     </div>
                     <div class="item-amount ${type === '+' ? 'income-amount':'expense-amount'}">
@@ -39,4 +41,19 @@ function resetForm() {
     document.querySelector('.add__type').value = '+';
     document.querySelector('.add__description').value = '';
     document.querySelector('.add__value').value = '';
+}
+
+//Utitlity function
+function getFormattedTime() {
+    const now = new Date().toLocaleTimeString('en-us', {
+        day: 'numeric',
+        month: 'short',
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+    const date = now.split(',')[0].split(' ');
+    const time = now.split(',')[1];
+    const formattedTime = `${date[1]} ${date[0]},${time}`;
+
+    return formattedTime;
 }
